8a2164013907c9cff34fe47c483c257732126b97
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2024-03-10 16:42:39 +0100

    ajout package AST

---
+++ src/AST/Div.java
@@ -0,0 +1,12 @@
+package AST;
+
+public class Div extends ExpressionA_Binaire{
+
+    public Div(ExpressionA gauche,ExpressionA droite){
+        super(gauche, droite);
+    }
+
+    public String symbole(){
+        return "diviser";
+    }; 
+}
