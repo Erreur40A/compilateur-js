a41eae283aac18f7176e5d6bf9fd5f517e556595
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2024-03-20 18:42:14 +0100

    cor methode symbole + branche

--- src/AST/Egalite.java
+++ src/AST/Egalite.java
@@ -3,7 +3,7 @@ package AST;
 public class Egalite extends ExpressionA_Binaire{
     public Egalite(ExpressionA gauche,ExpressionA droite) {super(gauche, droite);}
     
-    public String symbole() {return "==";}
+    public String symbole() {return "egale";}
 
     public String toAssembly(){
         String asb=gauche.toAssembly() + "\n" +
