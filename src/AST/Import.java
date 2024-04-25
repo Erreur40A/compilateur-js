fae0a768b7c341983cda61d3b39d56ce861841a3
Mounir ARAB <12103781@f203-16.ig-edu.univ-paris13.fr>
2024-04-25 16:27:16 +0200

    Changements Import

--- src/AST/Import.java
+++ src/AST/Import.java
@@ -6,15 +6,43 @@ public class Import extends ExpressionA{
 
     //A FAIRE
     public Import(String com, String arg){
-        super(0);//0 pour l'instant mais a modifié
+        super(lineNo(arg));
         commande=com;
         argument=arg;
     }
 
     public String toAssembly(){
         //lire le fichier arg et retourner son contenu
-        String asb=null;
-
+	String asb=null;
+	StringBuilder res = new StringBuilder();
+	try{
+		FileReader fich = new FileReader(argument+".jsm");
+        	BufferedReader r = new BufferedReader(fich);
+        	
+		while((line = r.readline()) != null) {
+			res.append(line).append("\n");
+		}        		
+	}catch(IOException e) {
+		System.out.println("readLine: " + e.getMessage());
+	}
+	asb = res.toString();
         return asb;
+    }
+    
+    public int lineNo(String arg){
+    	int count = 0;
+    	try{
+    		File fich = new File(arg+".jsm");
+    		Scanner sc = new Scanner(fich);
+    		
+    		while(sc.hasNextLine()){
+    			sc.nextLine();
+    			count++;
+    		}
+    		sc.close();
+    	}catch(Exception e){
+    		e.getStackTrace();
+    	}
+    	return count;
     }
 }
