d43f7228a4f34a7ddaf033bca4c84d841915597e
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2024-05-10 20:21:33 +0200

    cor bug toAssembly() CallFonction.java

--- src/AST/CallFonction.java
+++ src/AST/CallFonction.java
@@ -17,7 +17,7 @@ public class CallFonction extends ExpressionA {
     public String toAssembly() {
         StringBuilder asb = new StringBuilder();
 
-        asb.append("DecVar ").append(nom).append("\n");
+        asb.append("GetVar ").append(nom).append("\n");
         asb.append("StCall\n");
         asb.append(arg.toAssembly());
         asb.append("Call");
