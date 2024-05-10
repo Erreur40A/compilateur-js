2d0e53beb3973818529fdede48e94504da36653c
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2024-05-10 20:05:33 +0200

    ajout appel de fonction

--- src/AST/Programe.java
+++ src/AST/Programe.java
@@ -2,28 +2,24 @@ package AST;
 
 import java.util.ArrayList;
 
-public class Programe extends ExpressionA{
+public class Programe extends ExpressionA {
     ArrayList<ExpressionA> commande;
 
-    public Programe(){
+    public Programe() {
         super(0);
         commande = new ArrayList<ExpressionA>();
     }
 
-    public Programe(ExpressionA eA){
-        super(0);
-        commande = new ArrayList<ExpressionA>();
-        commande.add(eA);
-    }
-
-    public boolean add(ExpressionA eA){
+    public boolean add(ExpressionA eA) {
         size += eA.size;
         return commande.add(eA);
     }
 
-    public String symbole() {return "programme";}
+    public String symbole() {
+        return "programme";
+    }
 
-    public String toAssembly(){
+    public String toAssembly() {
         String asb = "";
 
         for (ExpressionA eA : commande) {
@@ -33,14 +29,14 @@ public class Programe extends ExpressionA{
         return asb;
     }
 
-    public String toString(){
-        String res=symbole() + "(";
+    public String toString() {
+        String res = symbole() + "(";
 
         for (ExpressionA e : commande) {
             res += "\n" + e.toString();
         }
 
-        res+="\n)";
+        res += "\n)";
 
         return res;
     }
