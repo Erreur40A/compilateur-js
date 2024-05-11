9453b36fed5db2cf8ebf821f595a18b8745ef4a7
Jibril DOUZI <jibril.douzi@edu.univ-paris13.fr>
2024-05-11 17:27:51 +0200

    cor affectation

---
+++ src/AST/Mod.java
@@ -0,0 +1,20 @@
+package AST;
+
+public class Mod extends ExpressionA_Binaire {
+
+    public Mod(ExpressionA gauche, ExpressionA droite) {
+        super(gauche, droite);
+    }
+
+    public String symbole() {
+        return "mod";
+    }
+
+    public String toAssembly() {
+        String asb = gauche.toAssembly() + "\n" +
+                droite.toAssembly() + "\n" +
+                "ModuNb";
+        return asb;
+    }
+
+}
